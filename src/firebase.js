78ba7ec97c82f9d418f2c168134a21e2722e8c3a
Nilesh Ganpat Chavan <nileshchavan8451@gmail.com>
2020-08-28 16:21:16 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAOiiiivYE8mnGWplwBWYIL-A-Yd-5ma0E",
-  authDomain: "nc-chat-app.firebaseapp.com",
-  databaseURL: "https://nc-chat-app.firebaseio.com",
-  projectId: "nc-chat-app",
-  storageBucket: "nc-chat-app.appspot.com",
-  messagingSenderId: "1009379485175",
-  appId: "1:1009379485175:web:a5e4c9c6af79af9238faf2",
-  measurementId: "G-41V5FYL4SB",
+// Your config object........
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
